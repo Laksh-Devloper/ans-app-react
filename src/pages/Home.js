9b0b11ca2b9397d158d6633fd051fe23480ac16d
Laksh_coder <71703674+Laksh-Devloper@users.noreply.github.com>
2022-07-30 19:01:23 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Home extends React.Component {
+    render() {
+        return (
+            <body>
+                
+
+                        <h1>Avalanche Name Service</h1>
+                        <form action="">
+                            <input type="search" required />
+                                <i class="fa fa-search"></i>
+                        </form>
+
+                        <img id="img" src="./assets/images/1.png" alt="" />
+                            <img id="logo" src="./assets/images/footer.png" alt="" />
+                                <img id="logo2" src="./assets/images/logo2.png" />
+
+                                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                                    </body>
+                            )
+    }
+}
+
+                            export default Home;
